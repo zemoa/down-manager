5abd8ba1af463c85bec120390385f5afbc23da6c
zemoa <zemoa@netc.fr>
2023-01-07 22:49:10 +0100

    add the beginning of the style

---
+++ downmanager-front/src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import TopAppBar , {Row, Section, Title} from "@smui/top-app-bar";
+</script>
+
+<div class="main">
+    <div class="head">
+        <TopAppBar>
+            <Row>
+                <Section>
+                    <Title>Downmanager</Title>
+                </Section>
+            </Row>
+        </TopAppBar>
+    </div>
+    <div class="content">
+        <slot></slot>
+    </div>
+</div>
+
+<style lang="scss">
+    .main {
+        margin: -8px;
+        display: flex;
+        flex-wrap: wrap;
+        .head {
+ 
+        }
+        .content {
+            margin-top: 64px;
+            padding: 8px;
+        }
+    }
+</style>
